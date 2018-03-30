dff9a479f36d6a99ea069ee40e6eb071cbf29001
rajthalapathy <thlpthrj@gmail.com>
2018-03-30 19:58:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gokulpradeep/leavereport/DashboardFragActivity.java
@@ -0,0 +1,29 @@
+package com.example.gokulpradeep.leavereport;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Gokul pradeep on 30-03-2018.
+ */
+
+public class DashboardFragActivity  extends Fragment{
+    public DashboardFragActivity() {
+        super();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
